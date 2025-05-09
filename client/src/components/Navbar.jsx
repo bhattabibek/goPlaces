3a804876b12e9d9f4dd3fda4c855981d61c09890
Bibek Bhatta <bhattabbk1@gmail.com>
2025-05-09 21:16:23 +0545

    Adds Icon in the logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Link, Links } from "react-router-dom";
 import { IoHomeOutline } from "react-icons/io5";
+import { FaGlobe } from "react-icons/fa";
 
 
 const Navbar = () => {
   return (
     <>
       <nav>
-        <h1 className="flex justify-center  bg-amber-500 p-2">Welcome to GO Places.com</h1>
+        <h1 className="flex justify-center  bg-amber-500 p-2">Welcome to Go Places.com</h1>
         <div  className="flex gap-4 justify-between flex-wrap p-4 shadow mb-1">
-        <h1 className="text-5xl">GOPLACES.</h1>
+        <h1 className="text-5xl flex items-center gap-2">
+  G <FaGlobe /> PLACES.
+</h1>
           <div className="flex gap-4 justify-center"><Link to="/"><IoHomeOutline />Home</Link>
           <Link to="/About">Destinations</Link>
           <Link to={"/about"}>About Us</Link>
