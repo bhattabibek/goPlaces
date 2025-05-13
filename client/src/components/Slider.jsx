8486ded3be3b8061e45cccb586ca3c0d1aea0795
Bibek Bhatta <bhattabbk1@gmail.com>
2025-05-13 13:53:42 +0545

    Adds text overlay in Slider images

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -16,11 +16,11 @@ function AutoPlay() {
   };
 
   return (
-    <div className="slider-container  h-6 bg-opacity-50">
-      <Slider {...settings} >
+    <div className="slider-container h-6 bg-opacity-5 relative0">
+      <Slider {...settings} className="relative" >
         <div>
-        <img className="w-full h-[80vh] object-cover" src="/image.jpg" alt="Mountain"  />
-
+        <img className="w-full h-[80vh] relative object-cover" src="/image.jpg" alt="Mountain"  />
+        <div class="absolute inset-0 bg-black opacity-13"></div>
         </div>
         <div>
           <img className="w-full h-[80vh] object-cover" src="/mount.jpg"/>
@@ -33,6 +33,13 @@ function AutoPlay() {
         </div>
         
       </Slider>
+      <div className="flex flex-row">
+      <div class="absolute inset-0 text-white text-4xl font-bold flex items-center">
+  Welcome to GoPlaces.<br/> Here you will find<br/> attractive Destinations <br/>in Nepal for you next Holidays.
+  <br/><button className="border border-white rounded-3xl p-4">Explore Now</button>
+  </div>
+  </div>
+
     </div>
   );
 }
