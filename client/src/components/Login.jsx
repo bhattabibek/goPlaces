619055fa62a25339ef2f088db7274e1b93f015a3
Bibek Bhatta <bhattabbk1@gmail.com>
2025-05-13 20:58:52 +0545

    Large screen optimized

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -7,25 +7,25 @@ import { FaFacebook } from "react-icons/fa";
 const Login = () => {
   return (
     <>
-      <div className="flex flex-col m-20 w-114 h-80 rounded-lg gap-3 md:m-10 xl:m-100 xl:my-10 ">
+      <div className="flex flex-col m-10 w-50 h-70 rounded-lg gap-3 md:m-10 xl:m-140 xl:my-20 ">
          <h1 className="text-5xl flex items-center gap-2">
          G <FaGlobe /> PLACES.
        </h1>
         <InputField />
         <input
-          className="border bg-orange-100 px-20 rounded-4xl py-2" 
+          className="border bg-orange-100 px-20 rounded-4xl py-2 xl:px-15" 
           type="password"
           placeholder="Enter Password"
         />
         <LoginButton />
-        OR
-        <div className="flex flex-row gap-2">
-  <h2 className="flex items-center gap-2 border text-white bg-[#EA4335] border-gray-400 rounded-3xl px-4 py-2">
+        <h2 className="text-center">OR</h2>
+        <div className="flex flex-col gap-2">
+  <h2 className="flex items-center gap-2 border text-white bg-[#EA4335] border-gray-400 rounded-3xl px-4 py-2 xl:px-2  xl:w-80 ">
     <FaGoogle size={20} />
     Login with Google
   </h2>
 
-  <h2 className="flex items-center gap-2 border text-white bg-[#1877F2] border-gray-400 rounded-3xl px-4 py-2">
+  <h2 className="flex items-center gap-2 border text-white bg-[#1877F2] border-gray-400 rounded-3xl px-4 py-2 xl:px-3 xl:w-80 xl:text-center">
     <FaFacebook size={20} />
     Login with Facebook
   </h2>
@@ -33,7 +33,7 @@ const Login = () => {
 
 
         <p>Forgot Password</p>
-        <button className="bg-amber-500 p-3 rounded-4xl">Create New Account</button>
+        <button className="bg-amber-500 p-3 rounded-4xl  xl:w-80">Create New Account</button>
       </div>
     </>
   );
--- client/src/components/LoginButton.jsx
+++ client/src/components/LoginButton.jsx
@@ -4,7 +4,7 @@ const LoginButton = () => {
   return (
     <div>
       
-      <button className='border  bg-[#154c79] text-white py-2  px-7'>Login</button>
+      <button className='border  bg-[#154c79] text-white py-2  rounded-4xl xl:w-80 px-7'>Login</button>
     </div>
   )
 }
